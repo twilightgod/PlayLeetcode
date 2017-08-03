5de25aa3b92d4ebe31db2ba37c58a00c25cf1832
Rex Xiong <bychance@gmail.com>
2017-08-02 22:45:27 -0700

    1

---
+++ 1/1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var rest = target - nums[i];
+                if (dict.ContainsKey(rest))
+                {
+                    return new int[2] { dict[rest], i };
+                }
+                else
+                {
+                    dict[nums[i]] = i;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ret = new Solution().TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine(ret);
+        }
+    }
+}
