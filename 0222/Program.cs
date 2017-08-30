165d66c186e9af35dde54663f8ecfc4d84c4d94a
Rex Xiong <bychance@gmail.com>
2017-08-29 23:51:48 -0700

    submit

---
+++ 0222/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _0222
+{
+    /**
+ * Definition for a binary tree node. */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var LHeight = GetLeftHeight(root.left);
+            var RHeight = GetLeftHeight(root.right);
+            var sum = 0;
+
+            // Left tree is full
+            if (LHeight == RHeight)
+            {
+                sum += (1 << LHeight) - 1;
+                sum += CountNodes(root.right);
+            }
+            // Right tree is full
+            else
+            {
+                sum += (1 << RHeight) - 1;
+                sum += CountNodes(root.left);
+            }
+
+            // plus one for root itself
+            return sum + 1;
+        }
+
+        private int GetLeftHeight(TreeNode root)
+        {
+            var height = 0;
+
+            while (root != null)
+            {
+                height++;
+                root = root.left;
+            }
+
+            return height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0662/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _0662
+{
+    /**
+ * Definition for a binary tree node.*/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int WidthOfBinaryTree(TreeNode root)
+        {
+            int n = 1000;
+            var max = Enumerable.Repeat(-1, n).ToArray();
+            var min = Enumerable.Repeat(Int32.MaxValue, n).ToArray();
+
+            Traverse(root, 0, 0, min, max);
+
+            int ans = 0;
+            for (var i = 0; i < n; ++i)
+            {
+                ans = Math.Max(ans, max[i] - min[i] + 1);
+            }
+
+            return ans;
+        }
+
+        private void Traverse(TreeNode root, int depth, int nodenum, int[] min, int[] max)
+        {
+            if (nodenum == Int32.MaxValue)
+            {
+                return;
+            }
+            if (root == null)
+            {
+                return;
+            }
+
+            min[depth] = Math.Min(min[depth], nodenum);
+            max[depth] = Math.Max(max[depth], nodenum);
+
+            Traverse(root.left, depth + 1, nodenum * 2, min, max);
+            Traverse(root.right, depth + 1, nodenum * 2 + 1, min, max);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
