8b15003f8c3b4bb1d5382227e439b5a8fdef7906
Rex Xiong <bychance@gmail.com>
2017-09-21 21:31:35 -0700

    submit

---
+++ 0377/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _0377
+{
+    public class Solution
+    {
+        public int CombinationSum4(int[] nums, int target)
+        {
+            var f = new int[target + 1];
+            f[0] = 1;
+            for (var i = 1; i <= target; ++i)
+            {
+                f[i] = 0;
+                foreach (var num in nums)
+                {
+                    f[i] += i - num >= 0 ? f[i - num] : 0;
+                }
+            }
+
+            return f[target];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
