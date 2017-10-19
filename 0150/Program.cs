d93e6129bc6e3256e1018d3c258bb6a63337c115
Rex Xiong <bychance@gmail.com>
2017-10-18 20:44:53 -0700

    submit

---
+++ 0150/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0150
+{
+    public class Solution
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            var stack = new Stack<int>();
+            foreach (var str in tokens)
+            {
+                var IsOperation = false;
+                
+                switch(str)
+                {
+                    case "+":
+                        case "-":
+                        case "*":
+                        case "/":
+                        IsOperation = true;
+                        break;
+                        default:
+                            stack.Push(Int32.Parse(str));
+                            break;
+
+                }
+
+                if (IsOperation)
+                {
+                    var n2 = stack.Pop();
+                    var n1 = stack.Pop();
+                    var n3 = 0;
+                    switch(str)
+                    {
+                        case "+":
+                            n3 = n1 + n2;
+                            break;
+                            case "-":
+                            n3 = n1 - n2;
+                            break;
+                            case "*":
+                            n3 = n1 * n2;
+                            break;
+                            case "/":
+                            n3 = n1 / n2;
+                            break;
+                            default:
+                                break;
+
+                    }
+
+                    stack.Push(n3);
+                }
+            }
+
+            return stack.Peek();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0152/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _0152
+{
+    public class Solution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            var best = nums.Max();
+            
+            var pre_max = 1;
+            var pre_min = 1;
+
+            foreach (var num in nums)
+            {
+                var cur_max = Math.Max(pre_max * num, pre_min * num);
+                var cur_min = Math.Min(pre_max * num, pre_min * num);
+
+                if (cur_max < 1)
+                {
+                    pre_max = 1;
+                }
+                else
+                {
+                    pre_max = cur_max;
+                    best = Math.Max(best, cur_max);
+                }
+
+                if (cur_min >= 0)
+                {
+                    pre_min = 1;
+                }
+                else
+                {
+                    pre_min = cur_min;
+                }
+            }
+
+            return best;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
