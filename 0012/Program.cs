60fc153989960db5270b0c4dcdac11715a66b04d
Rex Xiong <bychance@gmail.com>
2017-11-12 17:42:09 -0800

    12, 13

---
+++ 0012/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0012
+{
+    public class Solution
+    {
+        public string IntToRoman(int num)
+        {
+            var dic = new Dictionary<int, Dictionary<int, string>>()
+            {
+                {
+                    0, new Dictionary<int, string>()
+                    {
+                    {'0', ""},
+                    {'1', "I"},
+                    {'2', "II"},
+                    {'3', "III"},
+                    {'4', "IV"},
+                    {'5', "V"},
+                    {'6', "VI"},
+                    {'7', "VII"},
+                    {'8', "VIII"},
+                    {'9', "IX"},
+                    }
+                },
+
+                {
+                    1, new Dictionary<int, string>()
+                    {
+                    {'0', ""},
+                    {'1', "X"},
+                    {'2', "XX"},
+                    {'3', "XXX"},
+                    {'4', "XL"},
+                    {'5', "L"},
+                    {'6', "LX"},
+                    {'7', "LXX"},
+                    {'8', "LXXX"},
+                    {'9', "XC"},
+                    }
+                },
+
+                {
+                    2, new Dictionary<int, string>()
+                    {
+                    {'0', ""},
+                    {'1', "C"},
+                    {'2', "CC"},
+                    {'3', "CCC"},
+                    {'4', "CD"},
+                    {'5', "D"},
+                    {'6', "DC"},
+                    {'7', "DCC"},
+                    {'8', "DCCC"},
+                    {'9', "CM"},
+                    }
+                },
+
+                {
+                    3, new Dictionary<int, string>()
+                    {
+                    {'0', ""},
+                    {'1', "M"},
+                    {'2', "MM"},
+                    {'3', "MMM"},
+                    }
+                },
+            };
+
+            var ansList = new List<string>();
+            var s = num.ToString();
+            for (var i = 0; i < s.Length; ++i)
+            {
+                ansList.Add(dic[i][s[s.Length - i - 1]]);
+            }
+            ansList.Reverse();
+            return String.Join("", ansList);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0013/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0013
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            var dic1 = new Dictionary<string, int>()
+            {
+                {"IV", 4},
+                {"IX", 9},
+                {"XL", 40},
+                {"XC", 90},
+                {"CD", 400},
+                {"CM", 900},
+            };
+
+            var dic2 = new Dictionary<string, int>()
+            {
+                {"I", 1},
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000},
+            };
+
+            var ans = 0;
+
+            while (!String.IsNullOrEmpty(s))
+            {
+                var found = false;
+                foreach (var key in dic1.Keys)
+                {
+                    if (s.StartsWith(key))
+                    {
+                        ans += dic1[key];
+                        found = true;
+                        s = s.Substring(key.Length);
+                        break;            
+                    }
+                }
+
+                if (found)
+                {
+                    continue;
+                }
+
+                foreach (var key in dic2.Keys)
+                {
+                    if (s.StartsWith(key))
+                    {
+                        ans += dic2[key];
+                        found = true;
+                        s = s.Substring(key.Length);
+                        break;            
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
