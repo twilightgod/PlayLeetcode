317ea75ea37fac344d4bc17fb0c69c46e9267ee7
Rex Xiong <bychance@gmail.com>
2018-09-05 21:37:25 -0700

    submit

---
+++ 0076/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0076
+{
+    public class Solution
+    {
+        public string MinWindow(string s, string t)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                return String.Empty;
+            }
+
+            var goal = new Dictionary<char, int>();
+            var current = new Dictionary<char, int>();
+            var bestlen = Int32.MaxValue;
+            var bestpos = -1;
+
+            foreach (var c in t)
+            {
+                if (goal.ContainsKey(c))
+                {
+                    goal[c]++;
+                }
+                else
+                {
+                    goal[c] = 1;
+                    current[c] = 0;
+                }
+            }
+
+            var l = 0;
+            var r = 0;
+            if (goal.ContainsKey(s[l]))
+            {
+                current[s[l]]++;
+            }
+
+            while (r < s.Length)
+            {
+                if (FoundAnswer(goal, current))
+                {
+                    // compare overall best
+                    if (bestlen > r - l + 1)
+                    {
+                        bestlen = r - l + 1;
+                        bestpos = l;
+                    }
+
+                    // move left
+                    if (goal.ContainsKey(s[l]))
+                    {
+                        current[s[l]]--;
+                    }
+                    l++;
+                }
+                else
+                {
+                    // move right
+                    r++;
+                    if (r < s.Length && goal.ContainsKey(s[r]))
+                    {
+                        current[s[r]]++;
+                    }
+                }
+            }
+
+            return bestpos == -1 ? String.Empty : s.Substring(bestpos, bestlen);
+        }
+
+        private bool FoundAnswer(Dictionary<char, int> goal, Dictionary<char, int> current)
+        {
+            foreach (var key in goal.Keys)
+            {
+                if (goal[key] > current[key])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new Solution().MinWindow("ABC", "AC");
+        }
+    }
+}
---
+++ 0091/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0091
+{
+    public class Solution {
+    public int NumDecodings(string s) {
+        /*
+        if (String.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+        */
+        var f = Enumerable.Repeat(0, s.Length).ToList();
+
+        if (s[0] == '0')
+        {
+            return 0;
+        }
+        else
+        {
+            f[0] = 1;
+        }
+
+        for (var i = 1; i < s.Length; ++i)
+        {
+            if (s[i] != '0')
+            {
+                // decode ith char
+                f[i] = f[i - 1];
+            }
+            // decode (i-1 ~ i)th chars
+            var digits = s.Substring(i - 1, 2);
+            var number = Convert.ToInt32(digits);
+            if (number >= 10 && number <= 26)
+            {
+                if (i - 1 == 0)
+                {
+                    f[i] += 1;
+                }
+                else
+                {
+                    f[i] += f[i - 2];
+                }
+            }
+        }
+
+        return f[s.Length - 1];
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
