bb6d06bf50c937106b89a5a8c46b4af4088eb088
Rex Xiong <bychance@gmail.com>
2018-09-10 22:55:51 -0700

    submit

---
+++ 0365/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _0365
+{
+    public class Solution
+    {
+        public bool CanMeasureWater(int x, int y, int z)
+        {
+            if (x == 0)
+            {
+                return z == 0 || y == z;
+            }
+            if (y == 0)
+            {
+                return z == 0 || x == z;
+            }
+            var g = GCD(x, y);
+            return z >= 0 && z <= x + y && z % g == 0;
+        }
+
+        private int GCD(int x, int y)
+        {
+            while (y != 0)
+            {
+                var t = x % y;
+                x = y;
+                y = t;
+            }
+
+            return x;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0815/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0815
+{
+    public class Solution
+    {
+        public int NumBusesToDestination(int[][] routes, int S, int T)
+        {
+            var steps = new Dictionary<int, int>();
+            var routeByStop = new Dictionary<int, List<int>>();
+
+          for (var i = 0; i < routes.Length; ++i)
+            {
+               foreach (var stop in routes[i])
+                {
+                    steps[stop] = -1;
+                    if (!routeByStop.ContainsKey(stop))
+                    {
+                        routeByStop.Add(stop, new List<int>());
+                    }
+                    routeByStop[stop].Add(i);
+                }
+            }
+
+            var q = new Queue<int>();
+            q.Enqueue(S);
+            steps[S] = 0;
+
+            while (q.Count > 0)
+            {
+                var stop = q.Dequeue();
+                if (stop == T)
+                {
+                    break;
+                }
+
+                foreach (var route in routeByStop[stop])
+                {
+                    foreach (var destinationStop in routes[route])
+                    {
+                        if (steps[destinationStop] == -1)
+                        {
+                            steps[destinationStop] = steps[stop] + 1;
+                            q.Enqueue(destinationStop);
+                        }
+                    }
+                    // one route will only be checked once, since all stops in this route will be added to queue at the same time, huge performance improve here
+                    routes[route] = new int[]{};
+                }
+            }
+
+            return steps[T];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
