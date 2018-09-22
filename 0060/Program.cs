045b8fe9def5689d72d7d1428cbaef41adb14668
Rex Xiong <bychance@gmail.com>
2018-09-21 22:15:14 -0700

    submit

---
+++ 0060/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0060
+{
+    public class Solution
+    {
+        public string GetPermutation(int n, int k)
+        {
+            var nums = new List<int>();
+            for (var i = 1; i <= n; ++i)
+            {
+                nums.Add(i);
+            }
+
+            var factorials = new List<int>();
+            for (var i = 0; i <= 9; ++i)
+            {
+                if (i == 0)
+                {
+                    factorials.Add(1);
+                }
+                else
+                {
+                    factorials.Add(i * factorials[i - 1]);
+                }
+            }
+
+            // 0-based index
+            k--;
+
+            var sb = new StringBuilder();
+            for (var i = n; i >= 1; --i)
+            {
+                var group = k / factorials[i - 1];
+                k = k % factorials[i - 1];
+
+                sb.Append(nums[group]);
+                nums.RemoveAt(group);
+            }
+
+            return sb.ToString();
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0652/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0652
+{
+    /**
+ * Definition for a binary tree node.
+ */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+        {
+            // (val, left_tid, right_tid) -> tid
+            // key is very sparse, so can be expressed by int
+            var tidMapping = new Dictionary<long, int>();
+            // tree id -> counter
+            var counter = new Dictionary<int, int>();
+            var answers = new List<TreeNode>();
+
+            GetTid(root, tidMapping, counter, answers);
+
+            return answers;
+        }
+
+        private int GetTid(TreeNode root, Dictionary<long, int> tidMapping, Dictionary<int, int> counter, List<TreeNode> answers)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var treeFeature = (((long)root.val) << 32) | (GetTid(root.left, tidMapping, counter, answers) << 16) | GetTid(root.right, tidMapping, counter, answers);
+            if (!tidMapping.ContainsKey(treeFeature))
+            {
+                tidMapping[treeFeature] = tidMapping.Count + 1;
+            }
+            var tid = tidMapping[treeFeature];
+            if (!counter.ContainsKey(tid))
+            {
+                counter[tid] = 0;
+            }
+            if (++counter[tid] == 2)
+            {
+                answers.Add(root);
+            }
+
+            return tid;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
