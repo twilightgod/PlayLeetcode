e250db2790c4ac754aad20ff34950df3c68ea061
Rex Xiong <bychance@gmail.com>
2018-09-29 00:26:30 -0700

    submit

---
+++ 0681/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0681
+{
+    public class Solution
+    {
+        public string NextClosestTime(string time)
+        {
+            if (string.IsNullOrEmpty(time))
+            {
+                return time;
+            }
+
+            var digiset = new HashSet<int>();
+            var digilist = new List<int>();
+            var comboset = new HashSet<int>();
+            var combolist = new List<int>();
+            foreach (var c in time)
+            {
+                if (c != ':')
+                {
+                    digiset.Add((int)c - (int)'0');
+                }
+            }
+            if (digiset.Count == 1)
+            {
+                return time;
+            }
+            digilist = digiset.ToList();
+            foreach (var i in digilist)
+            {
+                foreach (var j in digilist)
+                {
+                    comboset.Add(i * 10 + j);
+                }
+            }
+            combolist = comboset.ToList();
+            combolist.Sort();
+
+            var hh = time.Substring(0, 2);
+            var mm = time.Substring(3, 2);
+            // case1, get next larger minute
+            var newmm = GetLarger(Int32.Parse(mm), 60, combolist);
+            var newhh = String.Empty;
+            if (!String.IsNullOrEmpty(newmm))
+            {
+                return hh + ":" + newmm;
+            }
+            else
+            {
+                // case2, get next larger hour, and smallest minute
+                newhh = GetLarger(Int32.Parse(hh), 24, combolist);
+                if (!String.IsNullOrEmpty(newhh))
+                {
+                    newmm = GetSmallest(60, combolist);
+                    return newhh + ":" + newmm;
+                }
+                else
+                {
+                    // case3, get smallest hour (next day) and smallest minute
+                    newhh = GetSmallest(24, combolist);
+                    newmm = GetSmallest(60, combolist);
+                    return newhh + ":" + newmm;
+                }                
+            }
+        }
+
+        private string GetLarger(int start, int limit, List<int> combolist)
+        {
+            foreach (var n in combolist)
+            {
+                if (n > start && n < limit)
+                {
+                    return n.ToString("D2");
+                }
+            }
+            return null;
+        }
+
+        private string GetSmallest(int limit, List<int> combolist)
+        {
+            foreach (var n in combolist)
+            {
+                if (n < limit)
+                {
+                    return n.ToString("D2");
+                }
+            }
+            return null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0687/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _0687
+{
+    /**
+ * Definition for a binary tree node.
+ */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        public int LongestUnivaluePath(TreeNode root)
+        {
+            return DFS(root).bestMax;
+        }
+
+        private (int bestMax, int currentMax) DFS(TreeNode root)
+        {
+            if (root == null)
+            {
+                return (0, 0);
+            }
+
+            var leftResult = DFS(root.left);
+            var rightResult = DFS(root.right);
+
+            var leftMax = root.left?.val == root.val ? leftResult.currentMax + 1 : 0; 
+            var rightMax = root.right?.val == root.val ? rightResult.currentMax + 1 : 0;
+            var currentMax = Math.Max(leftMax, rightMax);
+            var bestMax = Math.Max(Math.Max(leftResult.bestMax, rightResult.bestMax), leftMax + rightMax);
+
+            return (bestMax, currentMax);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
