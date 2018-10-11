51d7f8ec6ff7529fd02b859e14a75b870bb8c0af
Rex Xiong <bychance@gmail.com>
2018-10-11 00:17:55 -0700

    submit

---
+++ 0025/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace _0025
+{
+    /**
+ * Definition for singly-linked list.
+ */
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            // get total length
+            var n = 0;
+            var node = head;
+            while (node != null)
+            {
+                n++;
+                node = node.next;
+            }
+
+            var g = n / k;
+            if (g == 0)
+            {
+                return head;
+            }
+
+            // add newhead to keep first group the same logic
+            var newHead = new ListNode(0);
+            newHead.next = head;
+            var pre = newHead;
+            node = head;
+
+            for (var i = 0; i < g; ++i)
+            {
+                // save last node in previous group, it will link to last node in current group before reversing
+                var lastNodeInPreviousGroup = pre;
+                for (var j = 0; j < k; ++j)
+                {
+                    var next = node.next;
+                    node.next = pre;
+                    pre = node;
+                    node = next;
+                }
+                // last node in current group after reversing, it's actual last node in previous group's next
+                var lastNodeInCurrentGroup = lastNodeInPreviousGroup.next;
+                // pre is now first node in current group after reversing
+                lastNodeInPreviousGroup.next = pre;
+                // set pre to last node in current group
+                pre = lastNodeInCurrentGroup;
+                // link pre to node
+                pre.next = node;
+            }
+
+            return newHead.next;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new ListNode(1);
+            var node2 = new ListNode(2);
+            var node3 = new ListNode(3);
+            var node4 = new ListNode(4);
+            var node5 = new ListNode(5);
+
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+
+            new Solution().ReverseKGroup(node1, 2);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0857/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0857
+{
+    public class Worker : IComparable<Worker>
+    {
+        public int Quality;
+        public int Wage;
+        public double Ratio
+        {
+            get
+            {
+                return Wage * 1.0 / Quality;
+            }
+        }
+
+        public int CompareTo(Worker other)
+        {
+            return this.Ratio.CompareTo(other.Ratio);
+        }
+    }
+    public class Solution
+    {
+        public double MincostToHireWorkers(int[] quality, int[] wage, int K)
+        {
+            var n = quality.Length;
+            var workers = new Worker[n];
+            for (var i = 0; i < n; ++i)
+            {
+                workers[i] = new Worker() { Quality = quality[i], Wage = wage[i]};
+            }
+
+            Array.Sort(workers);
+
+            var answer = double.MaxValue;
+            // keep smallest top K
+            // add index for possible duplication in quality
+            var maxHeap = new SortedSet<(int, int)>(Comparer<(int, int)>.Create((a, b) => a.Item1 == b.Item1 ? a.Item2.CompareTo(b.Item2) : -a.Item1.CompareTo(b.Item1)));
+            var heapSum = 0;
+            for (var i = 0; i < n; ++i)
+            {
+                if (maxHeap.Count < K)
+                {
+                    heapSum += workers[i].Quality;
+                    maxHeap.Add((workers[i].Quality, i));
+                }
+                else
+                {
+                    var top = maxHeap.First();
+                    if (workers[i].Quality < top.Item1)
+                    {
+                        heapSum -= top.Item1;
+                        heapSum += workers[i].Quality;
+                        maxHeap.Remove(top);
+                        maxHeap.Add((workers[i].Quality, i));
+                    }
+                }
+
+                if (maxHeap.Count == K)
+                {
+                    answer = Math.Min(answer, heapSum * workers[i].Ratio);
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Solution().MincostToHireWorkers(new int[]{3,1,10,10,1}, new int[]{4,8,2,2,7}, 3);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
