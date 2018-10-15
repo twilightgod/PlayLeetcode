315dc101f44c8c93546ffcc403f707d9dee13d52
Rex Xiong <bychance@gmail.com>
2018-10-14 23:25:07 -0700

    submit

---
+++ 0402/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0402
+{
+    public class Solution
+    {
+        public string RemoveKdigits(string num, int k)
+        {
+            var n = num.Length;
+            if (n <= k)
+            {
+                return "0";
+            }
+
+            var stack = new Stack<char>();
+            foreach (var c in num)
+            {
+                while (k > 0 && stack.Count > 0 && stack.Peek() > c)
+                {
+                    stack.Pop();
+                    k--;
+                }
+                stack.Push(c);
+            }
+
+            // asc order
+            while (k > 0 && stack.Count > 0)
+            {
+                stack.Pop();
+                k--;
+            }
+
+            var answerArray = stack.ToArray();
+            Array.Reverse(answerArray);
+            var answerTrim = (new String(answerArray)).TrimStart(new char[]{'0'});
+            return answerTrim.Length == 0 ? "0" : answerTrim;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
