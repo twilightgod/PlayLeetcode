97a949e89382a30a36eb5ff004f31a460db336f6
Rex Xiong <bychance@gmail.com>
2018-10-23 23:03:29 -0700

    submit

---
+++ 0281/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0281
+{
+    public class ListWithPos
+    {
+        public IList<int> List;
+        public int Pos;
+        public ListWithPos(IList<int> list, int pos)
+        {
+            this.List = list;
+            this.Pos = pos;
+        }
+    }
+
+    public class ZigzagIterator
+    {
+        LinkedListNode<ListWithPos> CurrentListPointer = null;
+        LinkedList<ListWithPos> Lists = null;
+        public ZigzagIterator(IList<int> v1, IList<int> v2)
+        {
+            Lists = new LinkedList<ListWithPos>();
+            if (v1 != null && v1.Count > 0)
+            {
+                Lists.AddLast(new ListWithPos(v1, 0));
+            }
+            if (v2 != null && v2.Count > 0)
+            {
+                Lists.AddLast(new ListWithPos(v2, 0));
+            }
+            
+            CurrentListPointer = Lists.First;
+        }
+
+        public bool HasNext()
+        {
+            return CurrentListPointer != null;
+        }
+
+        public int Next()
+        {
+            var val = CurrentListPointer.Value.List[CurrentListPointer.Value.Pos];
+            var nextListPointer = CurrentListPointer.Next;
+            if (++CurrentListPointer.Value.Pos == CurrentListPointer.Value.List.Count)
+            {
+                Lists.Remove(CurrentListPointer);
+            }
+            CurrentListPointer = nextListPointer;
+            if (CurrentListPointer == null)
+            {
+                CurrentListPointer = Lists.First;
+            }
+            return val;
+        }
+    }
+
+    /**
+     * Your ZigzagIterator will be called like this:
+     * ZigzagIterator i = new ZigzagIterator(v1, v2);
+     * while (i.HasNext()) v[f()] = i.Next();
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0418/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _0418
+{
+    public class Solution
+    {
+        public int WordsTyping(string[] sentence, int rows, int cols)
+        {
+            var s = String.Join(" ", sentence);
+            s += " ";
+            var answer = 0;
+            var len = s.Length;
+            var p = 0;
+            for (var i = 0; i < rows; ++i)
+            {
+                var newp = (p + cols) % len;
+                var times = (p + cols) / len;
+                // adjust newp
+                // if it stops at non-space char, need to go back to last space, and add 1 for next round
+                while (s[newp] != ' ')
+                {
+                    newp--;
+                    if (newp == -1)
+                    {
+                        newp = len - 1;
+                        times--;
+                    }
+                }
+
+                if (++newp == len)
+                {
+                    times++;
+                    newp = 0;
+                }
+
+                p = newp;
+                answer += times;
+
+                // optimization for loop
+                if (p == 0)
+                {
+                    answer = answer * rows / (i + 1);
+                    break;
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
