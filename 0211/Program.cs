ec5bf047dee6464dc8c5b047b2b3f894b76cd715
Rex Xiong <bychance@gmail.com>
2018-10-26 23:12:04 -0700

    submit

---
+++ 0211/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0211
+{
+    public class TrieNode
+    {
+        public bool IsWord = false;
+        public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
+    }
+
+    public class WordDictionary
+    {
+        private TrieNode root = new TrieNode();
+
+        /** Initialize your data structure here. */
+        public WordDictionary()
+        {
+
+        }
+
+        /** Adds a word into the data structure. */
+        public void AddWord(string word)
+        {
+            var node = root;
+            foreach (var c in word)
+            {
+                if (!node.Children.ContainsKey(c))
+                {
+                    node.Children.Add(c, new TrieNode());
+                }
+                node = node.Children[c];
+            }
+            node.IsWord = true;
+        }
+
+        /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+        public bool Search(string word)
+        {
+            return DFS(word, 0, root);
+        }
+
+        private bool DFS(string s, int depth, TrieNode node)
+        {
+            if (depth == s.Length)
+            {
+                return node.IsWord;
+            }
+            if (s[depth] == '.')
+            {
+                foreach (var child in node.Children.Values)
+                {
+                    if (DFS(s, depth + 1, child))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                if (!node.Children.ContainsKey(s[depth]))
+                {
+                    return false;
+                }
+                return DFS(s, depth + 1, node.Children[s[depth]]);
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Your WordDictionary object will be instantiated and called as such:
+     * WordDictionary obj = new WordDictionary();
+     * obj.AddWord(word);
+     * bool param_2 = obj.Search(word);
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0745/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0745
+{
+    public class TrieNode
+    {
+        public List<int> WeightList = new List<int>();
+        public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
+    }
+
+    public class WordFilter
+    {
+        TrieNode suffixTree = new TrieNode();
+        TrieNode prefixTree = new TrieNode();
+
+        public WordFilter(string[] words)
+        {
+            for (var i = 0; i < words.Length; ++i)
+            {
+                var node = prefixTree;
+                node.WeightList.Add(i);
+                foreach (var c in words[i])
+                {
+                    if (!node.Children.ContainsKey(c))
+                    {
+                        node.Children.Add(c, new TrieNode());
+                    }
+                    node = node.Children[c];
+                    node.WeightList.Add(i);
+                }
+                node = suffixTree;
+                node.WeightList.Add(i);
+                for (var j = words[i].Length - 1; j >= 0; --j)
+                {
+                    var c = words[i][j];
+                    if (!node.Children.ContainsKey(c))
+                    {
+                        node.Children.Add(c, new TrieNode());
+                    }
+                    node = node.Children[c];
+                    node.WeightList.Add(i);
+                }
+            }
+        }
+
+        public int F(string prefix, string suffix)
+        {
+            List<int> prefixWordList = null;
+            List<int> suffixWordList = null;
+            var node = prefixTree;
+            foreach (var c in prefix)
+            {
+                if (!node.Children.ContainsKey(c))
+                {
+                    return -1;
+                }
+                node = node.Children[c];
+            }
+            prefixWordList = node.WeightList;
+
+            node = suffixTree;
+            for (var i = suffix.Length - 1; i >= 0; --i)
+            {
+                var c = suffix[i];
+                if (!node.Children.ContainsKey(c))
+                {
+                    return -1;
+                }
+                node = node.Children[c];
+            }
+            suffixWordList = node.WeightList;
+
+            if (suffixWordList.Count == 0 || prefixWordList.Count == 0)
+            {
+                return -1;
+            }
+
+            var p1 = prefixWordList.Count - 1;
+            var p2 = suffixWordList.Count - 1;
+            while (p1 >= 0 && p2 >= 0)
+            {
+                if (prefixWordList[p1] == suffixWordList[p2])
+                {
+                    return prefixWordList[p1];
+                }
+                else if (prefixWordList[p1] > suffixWordList[p2])
+                {
+                    p1--;
+                }
+                else
+                {
+                    p2--;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
