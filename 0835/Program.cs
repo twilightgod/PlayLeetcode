7f6d07048e89bb684417c7a16821a7c5a7aff648
Rex Xiong <bychance@gmail.com>
2018-10-28 13:50:01 -0700

    submit

---
+++ 0835/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _0835
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int LargestOverlap(int[][] A, int[][] B)
+            {
+                var answer = 0;
+                var n = A.GetLength(0);
+                var counter = new int[(2 * n) * (2 * n)];
+                for (var ia = 0; ia < n; ++ia)
+                {
+                    for (var ja = 0; ja < n; ++ja)
+                    {
+                        if (A[ia][ja] == 1)
+                        {
+                            for (var ib = 0; ib < n; ++ib)
+                            {
+                                for (var jb = 0; jb < n; ++jb)
+                                {
+                                    if (B[ib][jb] == 1)
+                                    {
+                                        counter[(ia - ib + n - 1) * 2 * n + (ja - jb + n - 1)]++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach (var c in counter)
+                {
+                    answer = Math.Max(answer, c);
+                }
+                return answer;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0853/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0853
+{
+    public class Car
+    {
+        public int Position;
+        public int Speed;
+        public double Time;
+    }
+
+    public class Solution
+    {
+        public int CarFleet(int target, int[] position, int[] speed)
+        {
+            if (position == null || position.Length == 0)
+            {
+                return 0;
+            }
+
+            var n = position.Length;
+            var cars = new List<Car>(n);
+            for (var i = 0; i < n; ++i)
+            {
+                cars.Add(new Car(){Position = position[i], Speed = speed[i], Time = (target - position[i]) * 1d / speed[i]});
+            }
+            cars.Sort((a, b) => -a.Position.CompareTo(b.Position));
+            var answer = 1;
+            var leader = cars[0];
+            for (var i = 1; i < n; ++i)
+            {
+                if (cars[i].Time > leader.Time)
+                {
+                    answer++;
+                    leader = cars[i];
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
