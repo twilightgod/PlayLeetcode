55bbfae5608ba26ec135288a8826b354dd038a9a
Rex Xiong <bychance@gmail.com>
2018-11-09 00:19:39 -0800

    submit

---
+++ 0678/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _0678
+{
+    public class Solution
+    {
+        public bool CheckValidString(string s)
+        {
+            // like the simple problem without *, counter increase by 1 when we meet (, decrease by 1 when meet )
+            // when introducing *, counter becomes a range, keep track of the left and right boundries
+            var l = 0;
+            var r = 0;
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    l++;
+                }
+                else
+                {
+                    l--;
+                }
+                if (c != ')')
+                {
+                    r++;
+                }
+                else
+                {
+                    r--;
+                }
+                if (r < 0)
+                {
+                    return false;
+                }
+                if (l < 0)
+                {
+                    l = 0;
+                }
+            }
+            return l == 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0723/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0723
+{
+    public class Solution
+    {
+        public int[,] CandyCrush(int[,] board)
+        {
+            var m = board.GetLength(0);
+            var n = board.GetLength(1);
+            while (true)
+            {
+                var found = false;
+                for (var i = 0; i < m; ++i)
+                {
+                    for (var j = 0; j < n; ++j)
+                    {
+                        var c = Math.Abs(board[i, j]);
+                        if (c > 0)
+                        {
+                            if (j < n - 2)
+                            {
+                                if (Math.Abs(board[i, j + 1]) == c && Math.Abs(board[i, j + 2]) == c)
+                                {
+                                    board[i, j] = -c;
+                                    board[i, j + 1] = -c;
+                                    board[i, j + 2] = -c;
+                                    found = true;
+                                }
+                            }
+                            if (i < m - 2)
+                            {
+                                if (Math.Abs(board[i + 1, j]) == c && Math.Abs(board[i + 2, j]) == c)
+                                {
+                                    board[i, j] = -c;
+                                    board[i + 1, j] = -c;
+                                    board[i + 2, j] = -c;
+                                    found = true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    break;
+                }
+
+                for (var j = 0; j < n; ++j)
+                {
+                    var last = m - 1;
+                    for (var i = m - 1; i >= 0; --i)
+                    {
+                        if (board[i, j] > 0)
+                        {
+                            board[last--, j] = board[i, j];
+                        }
+                    }
+                    while (last >= 0)
+                    {
+                        board[last--, j] = 0;
+                    }
+                }
+            }
+            return board;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
