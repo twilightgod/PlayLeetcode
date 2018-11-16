5c6c125a7a59629f91f4bed711e05585f3924f35
Rex Xiong <bychance@gmail.com>
2018-11-15 23:46:34 -0800

    submit

---
+++ 0773/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0773
+{
+    public class Solution
+    {
+        public int SlidingPuzzle(int[,] board)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in board)
+            {
+                sb.Append(c);
+            }
+            var start = sb.ToString();
+            var end = "123450";
+            var move = new int[]{1, -1, 3, -3};
+            var step = new Dictionary<string, int>();
+            var pre = new Dictionary<string, string>();
+            var q = new Queue<string>();
+            step.Add(start, 0);
+            pre[start] = start;
+            q.Enqueue(start);
+            while (q.Count > 0)
+            {
+                var cur = q.Dequeue();
+                if (cur == end)
+                {
+                    /*
+                    for (var p = end; pre[p] != p; p = pre[p])
+                    {
+                        Console.WriteLine(p);
+                    }
+                     */
+                    return step[cur];
+                }
+                var idx = cur.IndexOf('0');
+                for (var i = 0; i < 4; ++i)
+                {
+                    if (i == 0 && idx == 2 || i == 1 && idx == 3)
+                    {
+                        continue;
+                    }
+                    var target = idx + move[i];
+                    if (target >= 0 && target < 6)
+                    {
+                        var arr = cur.ToCharArray();
+                        arr[idx] = cur[target];
+                        arr[target] = '0';
+                        var next = new String(arr);
+                        if (!step.ContainsKey(next))
+                        {
+                            step.Add(next, step[cur] + 1);
+                            pre[next] = cur;
+                            q.Enqueue(next);
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0787/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0787
+{
+    public class Solution
+    {
+        public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+        {
+            var edge = new Dictionary<int, List<(int y, int d)>>();
+            // not only to record the shortest distance from src to each node
+            // but also needs another dimension of transit times
+            // it's possible to have shortest path can't meet the K limit
+            var dis = new int[n, K + 2];
+            for (var i = 0; i < n; ++i)
+            {
+                edge[i] = new List<(int y, int d)>();
+                for (var j = 0; j < K + 2; ++j)
+                {
+                    dis[i, j] = Int32.MaxValue;
+                }
+            }
+            for (var i = 0; i < flights.Length; ++i)
+            {
+                edge[flights[i][0]].Add((flights[i][1], flights[i][2]));
+            }
+            dis[src, 0] = 0;
+            // (distance, city, times)
+            var q = new SortedSet<(int d, int x, int t)>();
+            q.Add((0, src, 0));
+            while (q.Count > 0)
+            {
+                var node = q.First();
+                //Console.WriteLine(node);
+                q.Remove(node);
+                if (node.x == dst)
+                {
+                    return node.d;
+                }
+                if (node.t > K)
+                {
+                    continue;
+                }
+                foreach (var e in edge[node.x])
+                {
+                    var nextd = node.d + e.d;
+                    if (dis[e.y, node.t + 1] > nextd)
+                    {
+                        dis[e.y, node.t + 1] = nextd;
+                        q.Add((nextd, e.y, node.t + 1));
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
