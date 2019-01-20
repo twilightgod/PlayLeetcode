a6e1d0800f806a1f7e0c52bf9c8ad8e133723787
Rex Xiong <bychance@gmail.com>
2019-01-19 23:53:26 -0800

    submit

---
+++ 0735/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0735
+{
+    public class Solution
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            var stack = new Stack<int>();
+
+            foreach (var a in asteroids)
+            {
+                var ast = a;
+                while (stack.Count > 0 && stack.Peek() > 0 && ast < 0)
+                {
+                    if (-ast == stack.Peek())
+                    {
+                        stack.Pop();
+                        ast = 0;
+                    }
+                    else if (-ast > stack.Peek())
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        ast = 0;
+                    }
+                }
+                if (ast != 0)
+                {
+                    stack.Push(ast);
+                }
+            }
+
+            return stack.Reverse().ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0777/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0777
+{
+    public class Solution
+    {
+        public bool CanTransform(string start, string end)
+        {
+            if (start.Replace("X", "") != end.Replace("X", ""))
+            {
+                return false;
+            }
+
+            var L1 = new List<int>();
+            var L2 = new List<int>();
+            var R1 = new List<int>();
+            var R2 = new List<int>();
+
+            for (var i = 0; i < start.Length; ++i)
+            {
+                if (start[i] == 'L')
+                {
+                    L1.Add(i);
+                }
+                else if (start[i] == 'R')
+                {
+                    R1.Add(i);
+                }
+            }
+
+            for (var i = 0; i < end.Length; ++i)
+            {
+                if (end[i] == 'L')
+                {
+                    L2.Add(i);
+                }
+                else if (end[i] == 'R')
+                {
+                    R2.Add(i);
+                }
+            }
+
+            for (var i = 0; i < L1.Count; ++i)
+            {
+                if (L1[i] < L2[i])
+                {
+                    return false;
+                }
+            }
+
+            for (var i = 0; i < R1.Count; ++i)
+            {
+                if (R1[i] > R2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
