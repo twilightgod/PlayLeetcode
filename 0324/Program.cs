9b4506fb95e9b701f2fc2ed0a6dd96dc88f08906
Rex Xiong <bychance@gmail.com>
2019-02-15 00:20:08 -0800

    submit

---
+++ 0324/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _0324
+{
+    public class Solution
+    {
+        public void WiggleSort(int[] nums)
+        {
+            Array.Sort(nums);
+            var n = nums.Length;
+            var answer = new int[n];
+            var l = (n - 1) >> 1;
+            var r = n - 1;
+            for (var i = 0; i < n; ++i)
+            {
+                if ((i & 1) == 0)
+                {
+                    answer[i] = nums[l--];
+                }
+                else
+                {
+                    answer[i] = nums[r--];
+                }
+            }
+            for (var i = 0; i < n; ++i)
+            {
+                nums[i] = answer[i];
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
