d37b73261079f40da89fa1201830aaa47f7b1e94
Rex Xiong <bychance@gmail.com>
2019-02-21 23:53:44 -0800

    submit

---
+++ 0516/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _0516
+{
+    public class Solution
+    {
+        public int LongestPalindromeSubseq(string s)
+        {
+            var n = s.Length;
+            // f[i, j] means the length of longest palindrome in s[i..j]
+            var f = new int[n, n];
+            
+            for (var len = 1; len <= n; ++len)
+            {
+                for (var i = 0; i <= n - len; ++i)
+                {
+                    var j = i + len - 1;
+                    if (i == j)
+                    {
+                        f[i, j] = 1;
+                    }
+                    else if (s[i] == s[j])
+                    {
+                        f[i, j] = f[i + 1, j - 1] + 2;
+                    }
+                    else
+                    {
+                        f[i, j] = Math.Max(f[i + 1, j], f[i, j - 1]);
+                    }
+                }
+            }
+
+            return f[0, n - 1];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0655/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0655
+{
+    /**
+ * Definition for a binary tree node.
+ */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public IList<IList<string>> PrintTree(TreeNode root)
+        {
+            var answer = new List<IList<string>>();
+            if (root == null)
+            {
+                return answer;
+            }
+            var h = GetHeight(root);
+            var w = (1 << h) - 1;
+
+            // place holder
+            for (var i = 0; i < h; ++i)
+            {
+                answer.Add(new List<string>());
+                for (var j = 0; j < w; ++j)
+                {
+                    answer[i].Add("");
+                }
+            }
+
+            Print(root, w >> 1, answer, 0, w >> 1);
+
+            return answer;
+        }
+
+        private int GetHeight(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            return Math.Max(GetHeight(root.left), GetHeight(root.right)) + 1;
+        }
+
+        private void Print(TreeNode root, int w, IList<IList<string>> answer, int row, int col)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Console.WriteLine($"{w}, {row}, {col}, {root.val}");
+            answer[row][col] = root.val.ToString();
+            Print(root.left, w >> 1, answer, row + 1, col - 1 - (w >> 1));
+            Print(root.right, w >> 1, answer, row + 1, col + 1 + (w >> 1));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
