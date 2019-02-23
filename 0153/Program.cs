7d649c76c017f727455c7ab450e12c9780088989
Rex Xiong <bychance@gmail.com>
2019-02-22 23:18:37 -0800

    submit

---
+++ 0153/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _0153
+{
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            return DFS(nums, 0, nums.Length - 1);
+        }
+
+        private int DFS(int[] nums, int l, int r)
+        {
+            // single element
+            if (l == r)
+            {
+                return nums[l];
+            }
+            // already sorted
+            else if (nums[l] < nums[r])
+            {
+                return nums[l];
+            }
+            // devide and conquer, at least one of the two parts is sorted
+            else
+            {
+                var m = (l + r) / 2;
+                return Math.Min(DFS(nums, l, m), DFS(nums, m + 1, r));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [5,1,2,3,4]
+            // [4,5,1,2,3]
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0267/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0267
+{
+    public class Solution
+    {
+        public IList<string> GeneratePalindromes(string s)
+        {
+            var n = s.Length;
+            var cnt = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                cnt[c] = cnt.GetValueOrDefault(c) + 1;
+            }
+            var answers = new List<string>();
+            var answer = new char[n];
+            if (IsPossible(cnt))
+            {
+                DFS(0, n, cnt, answer, answers);
+            }
+            return answers;
+        }
+
+        private bool IsPossible(Dictionary<char, int> cnt)
+        {
+            var oddCnt = 0;
+            foreach (var value in cnt.Values)
+            {
+                if (value % 2 == 1)
+                {
+                    if (++oddCnt > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void DFS(int depth, int n, Dictionary<char, int> cnt, char[] answer, List<string> answers)
+        {
+            // only need to fill half
+            if (depth == n / 2)
+            {
+                // fill in last one for odd length
+                if (n % 2 == 1)
+                {
+                    foreach (var kvp in cnt)
+                    {
+                        if (kvp.Value != 0)
+                        {
+                            answer[depth] = kvp.Key;
+                            break;
+                        }
+                    }
+                }
+                answers.Add(new string(answer));
+                return;
+            }
+
+            // need to make a copy since Dictionary can't be changed during loop
+            var cnt_copied = new Dictionary<char, int>(cnt);
+            foreach (var kvp in cnt)
+            {
+                if (kvp.Value >= 2)
+                {
+                    answer[depth] = kvp.Key;
+                    answer[n - depth - 1] = kvp.Key;
+                    cnt_copied[kvp.Key] -= 2;
+                    DFS(depth + 1, n, cnt_copied, answer, answers);
+                    cnt_copied[kvp.Key] += 2;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
