7b5572cf9f137b801d89668f6497ed9c7b04772e
Rex Xiong <bychance@gmail.com>
2019-02-26 18:27:56 -0800

    submit

---
+++ 0097/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _0097
+{
+    public class Solution
+    {
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            // padding for empty string
+            s1 = "|" + s1;
+            s2 = "|" + s2;
+            s3 = "||" + s3;
+            var l1 = s1.Length;
+            var l2 = s2.Length;
+            var l3 = s3.Length;
+            if (l1 + l2 != l3)
+            {
+                return false;
+            }
+            
+            // f[i, j] matching first i chars from s3, with first j chars from s1, so i - j chars from s2
+            // s3[i - 1] == s1[j - 1]    f[i, j] = f[i - 1, j - 1]   comes from s1
+            // s3[i - 1] == s2[i - j - 1]     f[i, j] = f[i - 1, j]
+            // could use rolling array to reduce space complexity to O(n)
+            var f = new bool[l3 + 1, l1 + 1];
+            f[1, 1] = s1[0] == s3[0];
+            f[1, 0] = s2[0] == s3[0];
+            for (var i = 2; i <= l3; ++i)
+            {
+                for (var j = 0; j <= i && j <= l1; ++j)
+                {
+                    if (i - j <= l2)
+                    {
+                        if (j - 1 >= 0 && s1[j - 1] == s3[i - 1])
+                        {
+                            f[i, j] |= f[i - 1, j - 1];
+                        }
+                        if (i - j - 1 >= 0 && s2[i - j - 1] == s3[i - 1])
+                        {
+                            f[i, j] |= f[i - 1, j];
+                        }
+                        if (i == l3 && f[i, j])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Solution().IsInterleave("a", "b", "a");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0470/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _0470
+{
+    /**
+ * The Rand7() API is already defined in the parent class SolBase.
+ * public int Rand7();
+ * @return a random integer in the range 1 to 7
+ */
+    public class Solution : SolBase
+    {
+        public int Rand10()
+        {
+            /*
+                Run rand7 twice
+                7 * 7 = 49
+                49 / 10 = 4
+                49 % 10 = 9
+                discard last 9 entries
+             */
+
+            while (true)
+            {
+                var r1 = Rand7();
+                var r2 = Rand7();
+                // x is [0, 48]
+                var x = (r1 - 1) * 7 + (r2 - 1);
+                if (x >= 40)
+                {
+                    continue;
+                }
+                return x % 10 + 1;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
