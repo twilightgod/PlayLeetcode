c0458fbe93cdc083e3c814dfabf2ccf9170f3108
Rex Xiong <bychance@gmail.com>
2019-03-04 23:11:35 -0800

    submit

---
+++ 0544/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _0544
+{
+    public class Solution
+    {
+        public string FindContestMatch(int n)
+        {
+            var answer = new string[n];
+            for (var i = 1; i <= n; ++i)
+            {
+                answer[i - 1] = i.ToString();
+            }
+            while (n > 0)
+            {
+                for (var i = 0; i < n / 2; ++i)
+                {
+                    answer[i] = $"({answer[i]},{answer[n - i - 1]})";
+                }
+                n /= 2;
+            }
+            return answer[0];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
