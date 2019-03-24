23d7ccd9017edd4dece3b4c52b0fa5cfd2339b8b
Rex Xiong <bychance@gmail.com>
2019-03-23 23:27:58 -0700

    submit

---
+++ 0763/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0763
+{
+    public class Solution
+    {
+        public IList<int> PartitionLabels(string S)
+        {
+            var answer = new List<int>();
+            var lastPos = new Dictionary<char, int>();
+            
+            for (var i = 0; i < S.Length; ++i)
+            {
+                lastPos[S[i]] = i;
+            }
+
+            var start = 0;
+            var end = 0;
+            for (var i = 0; i < S.Length; ++i)
+            {
+                end = Math.Max(end, lastPos[S[i]]);
+                if (i == end)
+                {
+                    answer.Add(end - start + 1);
+                    start = i + 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0895/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0895
+{
+    public class FreqStack
+    {
+        int maxFreq = 0;
+        Dictionary<int, int> freq;
+        Dictionary<int, Stack<int>> stacks;
+
+        public FreqStack()
+        {
+            maxFreq = 0;
+            freq = new Dictionary<int, int>();
+            stacks = new Dictionary<int, Stack<int>>();
+        }
+
+        public void Push(int x)
+        {
+            var f = freq.GetValueOrDefault(x) + 1;
+            freq[x] = f;
+            if (!stacks.ContainsKey(f))
+            {
+                stacks[f] = new Stack<int>();
+            }
+            stacks[f].Push(x);
+            maxFreq = Math.Max(maxFreq, f);
+        }
+
+        public int Pop()
+        {
+            var x = stacks[maxFreq].Pop();
+            freq[x] = freq[x] - 1;
+            if (stacks[maxFreq].Count == 0)
+            {
+                maxFreq--;
+            }
+            return x;
+        }
+    }
+
+    /**
+     * Your FreqStack object will be instantiated and called as such:
+     * FreqStack obj = new FreqStack();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
