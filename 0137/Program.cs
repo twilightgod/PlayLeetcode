79bd9da3f1fdcfeb87855e38c19f82bfc370fbea
Rex Xiong <bychance@gmail.com>
2019-03-25 23:09:55 -0700

    submit

---
+++ 0137/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _0137
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var once = 0;
+            var twice = 0;
+            foreach (var num in nums)
+            {
+                once = once ^ num & ~twice;
+                twice = twice ^ num & ~once;
+            }
+            return once;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
