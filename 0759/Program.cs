1e7bd2c6d47493db43addd6e886dd58dc463e799
Rex Xiong <bychance@gmail.com>
2019-03-30 10:45:21 -0700

    submit

--- 0759/Program.cs
+++ 0759/Program.cs
@@ -18,13 +18,13 @@ namespace _0759
     {
         public IList<Interval> EmployeeFreeTime(IList<IList<Interval>> schedule)
         {
-            var events = new List<(int x, bool type)>();
+            var events = new List<(int x, int type)>();
             foreach (var employee in schedule)
             {
                 foreach (var interval in employee)
                 {
-                    events.Add((interval.start, false));
-                    events.Add((interval.end, true));
+                    events.Add((interval.start, 1));
+                    events.Add((interval.end, -1));
                 }
             }
             events.Sort();
@@ -33,20 +33,16 @@ namespace _0759
             var answers = new List<Interval>();
             foreach (var e in events)
             {
-                if (!e.type)
+                counter += e.type;
+                if (e.type == -1 && counter == 0)
                 {
-                    if (counter == 0 && lastIntervalEnd != -1)
+                    lastIntervalEnd = e.x;
+                }
+                else if (lastIntervalEnd != -1 && e.type == 1 && counter == 1)
+                {
+                    if (lastIntervalEnd != e.x)
                     {
                         answers.Add(new Interval(lastIntervalEnd, e.x));
-                    }
-                    counter++;
-                }
-                else
-                {
-                    counter--;
-                    if (counter == 0)
-                    {
-                        lastIntervalEnd = e.x;
                     }
                 }
             }
